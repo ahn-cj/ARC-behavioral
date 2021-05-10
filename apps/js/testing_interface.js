377567c5f8f36a989e0219d34577a36461cfcf3c
ahn-cj <54859344+ahn-cj@users.noreply.github.com>
2021-05-10 15:23:56 -0400

    Add files via upload

---
+++ apps/js/testing_interface.js
@@ -0,0 +1,412 @@
+
+// Internal state.
+var CURRENT_INPUT_GRID = new Grid(3, 3);
+var CURRENT_OUTPUT_GRID = new Grid(3, 3);
+var TEST_PAIRS = new Array();
+var CURRENT_TEST_PAIR_INDEX = 0;
+var COPY_PASTE_DATA = new Array();
+var TASK_ID = "";
+
+// Cosmetic.
+var EDITION_GRID_HEIGHT = 500;
+var EDITION_GRID_WIDTH = 500;
+var MAX_CELL_SIZE = 100;
+
+// PDDL
+var ACTION_COUNT = 0;
+var PDDL = [];
+
+function resetTask() {
+    CURRENT_INPUT_GRID = new Grid(3, 3);
+    TEST_PAIRS = new Array();
+    CURRENT_TEST_PAIR_INDEX = 0;
+    $('#task_preview').html('');
+    resetOutputGrid(false);
+}
+
+function refreshEditionGrid(jqGrid, dataGrid) {
+    fillJqGridWithData(jqGrid, dataGrid);
+    setUpEditionGridListeners(jqGrid);
+    fitCellsToContainer(jqGrid, dataGrid.height, dataGrid.width, EDITION_GRID_HEIGHT, EDITION_GRID_HEIGHT);
+    initializeSelectable();
+}
+
+function syncFromEditionGridToDataGrid() {
+    copyJqGridToDataGrid($('#output_grid .edition_grid'), CURRENT_OUTPUT_GRID);
+}
+
+function syncFromDataGridToEditionGrid() {
+    refreshEditionGrid($('#output_grid .edition_grid'), CURRENT_OUTPUT_GRID);
+}
+
+function getSelectedSymbol() {
+    selected = $('#symbol_picker .selected-symbol-preview')[0];
+    return $(selected).attr('symbol');
+}
+
+function setUpEditionGridListeners(jqGrid) {
+    jqGrid.find('.cell').click(function(event) {
+        cell = $(event.target);
+        symbol = getSelectedSymbol();
+
+        mode = $('input[name=tool_switching]:checked').val();
+        if (mode == 'floodfill') {
+            // If floodfill: fill all connected cells.
+            syncFromEditionGridToDataGrid();
+            grid = CURRENT_OUTPUT_GRID.grid;
+            floodfillFromLocation(grid, cell.attr('x'), cell.attr('y'), symbol);
+            syncFromDataGridToEditionGrid();
+
+            if (typeof symbol !== 'undefined')
+                PDDL.push("FILL " + cell.attr("x") + " " + cell.attr("y") + " " + symbol)
+        }
+        else if (mode == 'edit') {
+            // Else: fill just this cell.
+            setCellSymbol(cell, symbol);
+
+            if (typeof symbol !== 'undefined')
+                PDDL.push("EDIT " + cell[0].getAttribute("x") + " " + cell[0].getAttribute("y") + " " + symbol)
+        }
+    });
+}
+
+function resizeOutputGrid(from_ui) {
+    size = $('#output_grid_size').val();
+    size = parseSizeTuple(size);
+    height = size[0];
+    width = size[1];
+    jqGrid = $('#output_grid .edition_grid');
+    syncFromEditionGridToDataGrid();
+    dataGrid = JSON.parse(JSON.stringify(CURRENT_OUTPUT_GRID.grid));
+    CURRENT_OUTPUT_GRID = new Grid(height, width, dataGrid);
+    refreshEditionGrid(jqGrid, CURRENT_OUTPUT_GRID);
+
+    if (from_ui) PDDL.push("RESIZE " + CURRENT_OUTPUT_GRID['height'] + " " + CURRENT_OUTPUT_GRID['width']);
+}
+
+function resetOutputGrid(from_ui) {
+    syncFromEditionGridToDataGrid();
+    CURRENT_OUTPUT_GRID = new Grid(3, 3);
+    syncFromDataGridToEditionGrid();
+    resizeOutputGrid(false);
+
+    if (from_ui) PDDL.push('RESET_GRID')
+}
+
+function copyFromInput() {
+    syncFromEditionGridToDataGrid();
+    CURRENT_OUTPUT_GRID = convertSerializedGridToGridObject(CURRENT_INPUT_GRID.grid);
+    syncFromDataGridToEditionGrid();
+    $('#output_grid_size').val(CURRENT_OUTPUT_GRID.height + 'x' + CURRENT_OUTPUT_GRID.width);
+
+    PDDL.push('COPY_INPUT');
+}
+
+function fillPairPreview(pairId, inputGrid, outputGrid) {
+    var pairSlot = $('#pair_preview_' + pairId);
+    if (!pairSlot.length) {
+        // Create HTML for pair.
+        pairSlot = $('<div id="pair_preview_' + pairId + '" class="pair_preview" index="' + pairId + '"></div>');
+        pairSlot.appendTo('#task_preview');
+    }
+    var jqInputGrid = pairSlot.find('.input_preview');
+    if (!jqInputGrid.length) {
+        jqInputGrid = $('<div class="input_preview"></div>');
+        jqInputGrid.appendTo(pairSlot);
+    }
+    var jqOutputGrid = pairSlot.find('.output_preview');
+    if (!jqOutputGrid.length) {
+        jqOutputGrid = $('<div class="output_preview"></div>');
+        jqOutputGrid.appendTo(pairSlot);
+    }
+
+    fillJqGridWithData(jqInputGrid, inputGrid);
+    fitCellsToContainer(jqInputGrid, inputGrid.height, inputGrid.width, 200, 200);
+    fillJqGridWithData(jqOutputGrid, outputGrid);
+    fitCellsToContainer(jqOutputGrid, outputGrid.height, outputGrid.width, 200, 200);
+}
+
+function loadJSONTask(train) {
+    resetTask();
+    $('#modal_bg').hide();
+    $('#error_display').hide();
+    $('#info_display').hide();
+
+    for (var i = 0; i < train.length; i++) {
+        pair = train[i];
+        values = pair['input'];
+        input_grid = convertSerializedGridToGridObject(values)
+        values = pair['output'];
+        output_grid = convertSerializedGridToGridObject(values)
+        fillPairPreview(i, input_grid, output_grid);
+    }
+    values = TEST_PAIRS[0]['input'];
+    CURRENT_INPUT_GRID = convertSerializedGridToGridObject(values)
+    fillTestInput(CURRENT_INPUT_GRID);
+    CURRENT_TEST_PAIR_INDEX = 0;
+    $('#current_test_input_id_display').html('1');
+    $('#total_test_input_count_display').html(test.length);
+
+    initPDDL();
+}
+
+function loadTaskFromFile(e) {
+    var file = e.target.files[0];
+    TASK_ID = file['name'];
+    if (!file) {
+        errorMsg('No file selected');
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+
+        try {
+            contents = JSON.parse(contents);
+            train = contents['train'];
+        } catch (e) {
+            errorMsg('Bad file format');
+            return;
+        }
+        loadJSONTask(train);
+    };
+    reader.readAsText(file);
+}
+
+//figure out how to present tasks in order
+function randomTask() {
+    var subset = "training";
+//need to change source to my github...
+    $.getJSON("https://api.github.com/repos/ahn-cj/ARC-behavioral/demo-tasks" + subset, function(tasks) {
+      var task = tasks[Math.floor(Math.random() * tasks.length)];
+      TASK_ID = task['name'];
+      $.getJSON(task["download_url"], function(json) {
+          try {
+              train = json['train'];
+          } catch (e) {
+              errorMsg('Bad file format');
+              return;
+          }
+          loadJSONTask(train);
+          $('#load_task_file_input')[0].value = "";
+          infoMsg("Loaded task training/" + task["name"]);
+      })
+      .error(function(){
+        errorMsg('Error loading task');
+      });
+    })
+    .error(function(){
+      errorMsg('Error loading task list');
+    });
+}
+
+function nextTestInput() {
+    if (TEST_PAIRS.length <= CURRENT_TEST_PAIR_INDEX + 1) {
+        errorMsg('No next test input. Pick another file?')
+        return
+    }
+    CURRENT_TEST_PAIR_INDEX += 1;
+    values = TEST_PAIRS[CURRENT_TEST_PAIR_INDEX]['input'];
+    CURRENT_INPUT_GRID = convertSerializedGridToGridObject(values)
+    fillTestInput(CURRENT_INPUT_GRID);
+    $('#current_test_input_id_display').html(CURRENT_TEST_PAIR_INDEX + 1);
+    $('#total_test_input_count_display').html(test.length);
+}
+
+function submitSolution() {
+    syncFromEditionGridToDataGrid();
+    reference_output = TEST_PAIRS[CURRENT_TEST_PAIR_INDEX]['output'];
+    submitted_output = CURRENT_OUTPUT_GRID.grid;
+    if (reference_output.length != submitted_output.length) {
+        errorMsg('Wrong solution.');
+        return
+    }
+    for (var i = 0; i < reference_output.length; i++){
+        ref_row = reference_output[i];
+        for (var j = 0; j < ref_row.length; j++){
+            if (ref_row[j] != submitted_output[i][j]) {
+                errorMsg('Wrong solution.');
+                return
+//add code to reset output grid OR proceed to next task... need counter for tries
+            }
+        }
+
+    }
+    infoMsg('Correct solution!');
+}
+
+function fillTestInput(inputGrid) {
+    jqInputGrid = $('#evaluation_input');
+    fillJqGridWithData(jqInputGrid, inputGrid);
+    fitCellsToContainer(jqInputGrid, inputGrid.height, inputGrid.width, 400, 400);
+}
+
+function copyToOutput() {
+    syncFromEditionGridToDataGrid();
+    CURRENT_OUTPUT_GRID = convertSerializedGridToGridObject(CURRENT_INPUT_GRID.grid);
+    syncFromDataGridToEditionGrid();
+    $('#output_grid_size').val(CURRENT_OUTPUT_GRID.height + 'x' + CURRENT_OUTPUT_GRID.width);
+}
+
+function initializeSelectable() {
+    try {
+        $('.selectable_grid').selectable('destroy');
+        // PDDL.push("DESELECT")
+    }
+    catch (e) {
+    }
+    toolMode = $('input[name=tool_switching]:checked').val();
+    if (toolMode == 'select') {
+        infoMsg('Select some cells and click on a color to fill in, or press C to copy');
+        $('.selectable_grid').selectable(
+            {
+                autoRefresh: false,
+                filter: '> .row > .cell',
+                start: function(event, ui) {
+                    $('.ui-selected').each(function(i, e) {
+                        $(e).removeClass('ui-selected');
+                    });
+                }
+            }
+        );
+    }
+}
+
+// PDDL Action Tracking
+function downloadPDDL() {
+    var blob = new Blob([PDDL.join('\n')],
+                { type: "" });
+    saveAs(blob, TASK_ID.split('.')[0] + ".json");
+}
+
+function initPDDL() {
+    PDDL = [];
+    PDDL.push("GRID " + CURRENT_OUTPUT_GRID['height'] + " " + CURRENT_OUTPUT_GRID['width']);
+}
+
+// Initial event binding.
+
+$(document).ready(function () {
+    $('#symbol_picker').find('.symbol_preview').click(function(event) {
+        symbol_preview = $(event.target);
+        $('#symbol_picker').find('.symbol_preview').each(function(i, preview) {
+            $(preview).removeClass('selected-symbol-preview');
+        })
+        symbol_preview.addClass('selected-symbol-preview');
+
+        toolMode = $('input[name=tool_switching]:checked').val();
+        if (toolMode == 'select') {
+            $('.edition_grid').find('.ui-selected').each(function(i, cell) {
+                symbol = getSelectedSymbol();
+                setCellSymbol($(cell), symbol);
+
+                if (typeof symbol !== 'undefined')
+                    PDDL.push("SELECT_FILL " + " " + cell.getAttribute("x") + " " + cell.getAttribute("y") + " "+ symbol)
+            });
+        }
+    });
+
+    $('.edition_grid').each(function(i, jqGrid) {
+        setUpEditionGridListeners($(jqGrid));
+    });
+
+    $('.load_task').on('change', function(event) {
+        loadTaskFromFile(event);
+    });
+
+    $('.load_task').on('click', function(event) {
+      event.target.value = "";
+    });
+
+    $('input[type=radio][name=tool_switching]').change(function() {
+        initializeSelectable();
+    });
+
+    $('body').keydown(function(event) {
+        // Copy and paste functionality.
+        if (event.which == 67) {
+            // Press C
+
+            selected = $('.ui-selected');
+            if (selected.length == 0) {
+                return;
+            }
+
+            // Map Source/Target Value
+            source = selected[0].parentElement.parentElement.parentElement.getAttribute("id");
+
+            COPY_PASTE_DATA = [];
+            for (var i = 0; i < selected.length; i ++) {
+                x = parseInt($(selected[i]).attr('x'));
+                y = parseInt($(selected[i]).attr('y'));
+                symbol = parseInt($(selected[i]).attr('symbol'));
+                COPY_PASTE_DATA.push([x, y, symbol]);
+
+                var s = 0;
+                if (source == "evalution-input-view")
+                    s = 0;
+                else if (source == "output_grid")
+                    s = 1;
+                PDDL.push("SELECT_COPY "  + x + " " + y + " " + symbol + " " + s);
+            }
+            infoMsg('Cells copied! Select a target cell and press V to paste at location.');
+
+        }
+        if (event.which == 86) {
+            // Press P
+            if (COPY_PASTE_DATA.length == 0) {
+                errorMsg('No data to paste.');
+                return;
+            }
+            selected = $('.edition_grid').find('.ui-selected');
+            if (selected.length == 0) {
+                errorMsg('Select a target cell on the output grid.');
+                return;
+            }
+
+            jqGrid = $(selected.parent().parent()[0]);
+
+            // Map Source/Target Value
+            source = selected[0].parentElement.parentElement.parentElement.getAttribute("id");
+
+            if (selected.length == 1) {
+                targetx = parseInt(selected.attr('x'));
+                targety = parseInt(selected.attr('y'));
+
+                xs = new Array();
+                ys = new Array();
+                symbols = new Array();
+
+                for (var i = 0; i < COPY_PASTE_DATA.length; i ++) {
+                    xs.push(COPY_PASTE_DATA[i][0]);
+                    ys.push(COPY_PASTE_DATA[i][1]);
+                    symbols.push(COPY_PASTE_DATA[i][2]);
+                }
+
+                minx = Math.min(...xs);
+                miny = Math.min(...ys);
+                for (var i = 0; i < xs.length; i ++) {
+                    x = xs[i];
+                    y = ys[i];
+                    symbol = symbols[i];
+                    newx = x - minx + targetx;
+                    newy = y - miny + targety;
+                    res = jqGrid.find('[x="' + newx + '"][y="' + newy + '"] ');
+                    if (res.length == 1) {
+                        cell = $(res[0]);
+                        setCellSymbol(cell, symbol);
+
+                        var s = 0;
+                        if (source == "evalution-input-view")
+                            s = 0;
+                        else if (source == "output_grid")
+                            s = 1;
+                        PDDL.push("SELECT_PASTE "  + cell.attr('x') + " " + cell.attr('y') + " " + symbol + " " + s);
+                    }
+                }
+            } else {
+                errorMsg('Can only paste at a specific location; only select *one* cell as paste destination.');
+            }
+        }
+    });
+});
