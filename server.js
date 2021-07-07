09c25aaab4ad52cf598efff472e2857d092046b1
ahn-cj <54859344+ahn-cj@users.noreply.github.com>
2021-07-07 13:58:27 -0400

    Playing around with node.js

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public')); //__dir and not _dir
-var port = 8000; // you can use any port
-app.listen(port);
-console.log('server on' + port);
